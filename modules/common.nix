aa332c272710b09ad221732cc80e51706d22d41c
lucasbrt <lucasbritodepaula.t@gmail.com>
2025-10-26 12:55:55 -0300

    adiciona calculadora (finalmente)

--- modules/common.nix
+++ modules/common.nix
@@ -1,73 +1,85 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 
 {
-    # Bootloader
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-    # Timezone and Internalization
-    time.timeZone = "America/Fortaleza";
+  # Timezone and Internalization
+  time.timeZone = "America/Fortaleza";
 
-    # Setup experimental features
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Setup experimental features
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-    # System Packages
-    environment.systemPackages = with pkgs; [
-        fish
-        curl
-        openssl
+  # System Packages
+  environment.systemPackages = with pkgs; [
+    fish
+    curl
+    openssl
+    cosmic-ext-calculator
+    cosmic-ext-tweaks
+  ];
+
+  fonts.packages = with pkgs; [
+    fira-code
+    fira-code-symbols
+  ];
+
+  #Gaming extra configs
+  programs.steam = {
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    gamescopeSession.enable = true;
+  };
+  programs.gamemode.enable = true;
+
+  # Shell extra configs
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
+
+  # Allow non-free software
+  nixpkgs.config.allowUnfree = true;
+
+  # Services
+  services.openssh.enable = true;
+  virtualisation.docker = {
+    enable = true;
+  };
+  services.printing.enable = true;
+
+  # Hardware Graphics Aceleration
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
+
+  # Desktop Environment Settings
+  services.xserver.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
+  services.desktopManager.cosmic.enable = true;
+  services.desktopManager.cosmic.xwayland.enable = true;
+
+  # Sound Settings
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # User Account
+  users.users.lucasbrt = {
+    isNormalUser = true;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
     ];
+  };
 
- 
-
- #Gaming extra configs
-    programs.steam = {
-        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-        gamescopeSession.enable = true;
-    };
-    programs.gamemode.enable = true;
-
-    # Shell extra configs
-    programs.fish.enable = true;
-    users.defaultUserShell = pkgs.fish;
-
-    # Allow non-free software
-    nixpkgs.config.allowUnfree = true;
-
-    # Services
-    services.openssh.enable = true;
-    virtualisation.docker = {
-      enable = true;
-    };
-    services.printing.enable = true;
-
-    # Hardware Graphics Aceleration
-    hardware.graphics.enable = true;
-    hardware.graphics.enable32Bit = true;
-
-    # Desktop Environment Settings
-    services.xserver.enable = true;
-    services.displayManager.cosmic-greeter.enable = true;
-    services.desktopManager.cosmic.enable = true;
-    services.desktopManager.cosmic.xwayland.enable = true;
-
-    # Sound Settings
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-    };
-
-    # User Account
-    users.users.lucasbrt = {
-        isNormalUser = true;
-        extraGroups = [ "networkmanager" "wheel" "docker" ];
-    };
-
-    system.stateVersion = "25.05";
+  system.stateVersion = "25.05";
 
 }
