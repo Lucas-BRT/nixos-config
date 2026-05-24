654a87001e3af43752fdfa3c741016295ff9d0de
Lucas-BRT <lucasbritodepaula@proton.me>
2026-05-24 13:21:46 -0300

    usa a versão unstable dos pacotes do Cosmic

--- flake.nix
+++ flake.nix
@@ -3,16 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
+      pkgs-unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      specialArgs = { inherit inputs pkgs-unstable; };
 
       mkHost = hostPath: nixpkgs.lib.nixosSystem {
        	inherit system specialArgs;
--- modules/DE/cosmic.nix
+++ modules/DE/cosmic.nix
@@ -1,6 +1,37 @@
-{ ... }:
+{ pkgs-unstable, ... }:
 
 {
+  nixpkgs.overlays = [
+    (_: _: {
+      inherit (pkgs-unstable)
+        cosmic-applets
+        cosmic-applibrary
+        cosmic-bg
+        cosmic-comp
+        cosmic-edit
+        cosmic-files
+        cosmic-greeter
+        cosmic-icons
+        cosmic-idle
+        cosmic-launcher
+        cosmic-notifications
+        cosmic-osd
+        cosmic-panel
+        cosmic-player
+        cosmic-randr
+        cosmic-screenshot
+        cosmic-session
+        cosmic-settings
+        cosmic-settings-daemon
+        cosmic-store
+        cosmic-term
+        cosmic-wallpapers
+        cosmic-workspaces-epoch
+        pop-launcher
+        xdg-desktop-portal-cosmic;
+    })
+  ];
+
   services.desktopManager.cosmic.enable = true;
   services.desktopManager.cosmic.xwayland.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
